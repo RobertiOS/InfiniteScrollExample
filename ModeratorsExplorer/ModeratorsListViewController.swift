274e98b8b426254525a63b34337d97789fdca409
Robert <Robert.ios.developer@gmail.com>
2022-09-02 12:16:34 -0600

    :sparkles: added inifite scroll

--- ModeratorsExplorer/ModeratorsListViewController.swift
+++ ModeratorsExplorer/ModeratorsListViewController.swift
@@ -23,6 +23,7 @@ class ModeratorsListViewController: UIViewController, AlertDisplayer {
     tableView.isHidden = true
     tableView.separatorColor = ColorPalette.RWGreen
     tableView.dataSource = self
+    tableView.prefetchDataSource = self
     
     let request = ModeratorRequest.from(site: site)
     viewModel = ModeratorsViewModel(request: request, delegate: self)
@@ -33,21 +34,31 @@ class ModeratorsListViewController: UIViewController, AlertDisplayer {
 
 extension ModeratorsListViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return viewModel.currentCount
+    return viewModel.totalCount
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifiers.list, for: indexPath) as! ModeratorTableViewCell
-    cell.configure(with: viewModel.moderator(at: indexPath.row))
+    let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifiers.list,
+                                             for: indexPath) as! ModeratorTableViewCell
+    if isLoadingCell(for: indexPath) {
+      cell.configure(with: .none)
+    } else {
+      cell.configure(with: viewModel.moderator(at: indexPath.row))
+    }
     return cell
   }
 }
 
 extension ModeratorsListViewController: ModeratorsViewModelDelegate {
   func onFetchCompleted(with newIndexPathsToReload: [IndexPath]?) {
-    indicatorView.stopAnimating()
-    tableView.isHidden = false
-    tableView.reloadData()
+    guard let newIndexPathsToReload = newIndexPathsToReload else {
+      indicatorView.stopAnimating()
+      tableView.isHidden = false
+      tableView.reloadData()
+      return
+    }
+    let indexPathsToReload = visibleIndexPathsToReload(intersecting: newIndexPathsToReload)
+    tableView.reloadRows(at: indexPathsToReload, with: .automatic)
   }
   
   func onFetchFailed(with reason: String) {
@@ -56,5 +67,27 @@ extension ModeratorsListViewController: ModeratorsViewModelDelegate {
     let title = "Warning".localizedString
     let action = UIAlertAction(title: "OK".localizedString, style: .default)
     displayAlert(with: title , message: reason, actions: [action])
+  }
+}
+
+extension ModeratorsListViewController: UITableViewDataSourcePrefetching {
+
+  func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+    if indexPaths.contains(where: isLoadingCell) {
+        viewModel.fetchModerators()
+      }
+  }
+
+}
+
+private extension ModeratorsListViewController {
+  func isLoadingCell(for indexPath: IndexPath) -> Bool {
+    return indexPath.row >= viewModel.currentCount
+  }
+
+  func visibleIndexPathsToReload(intersecting indexPaths: [IndexPath]) -> [IndexPath] {
+    let indexPathsForVisibleRows = tableView.indexPathsForVisibleRows ?? []
+    let indexPathsIntersection = Set(indexPathsForVisibleRows).intersection(indexPaths)
+    return Array(indexPathsIntersection)
   }
 }
